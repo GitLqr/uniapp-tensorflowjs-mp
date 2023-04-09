0b1fb646418e145aa3aea9edfba97dea32fdc473
GitLqr <linqiarui@gmail.com>
2023-04-09 17:45:08 +0800

    feat: 配置 wasm 后端

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,20 @@ import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
 import { fetchFunc } from "fetch-wechat";
 import * as tf from "@tensorflow/tfjs-core";
 import * as webgl from "@tensorflow/tfjs-backend-webgl";
+import { setWasmPaths } from "@tensorflow/tfjs-backend-wasm";
 const plugin = requirePlugin("tfjsPlugin");
 
 const ENABLE_DEBUG = false;
+
+const usePlatformFetch = true;
+setWasmPaths(
+  {
+    "tfjs-backend-wasm.wasm": "/static/tfjs-backend-wasm.wasm",
+    "tfjs-backend-wasm-simd.wasm": "/static/tfjs-backend-wasm.wasm",
+    "tfjs-backend-wasm-threaded-simd.wasm": "/static/tfjs-backend-wasm.wasm",
+  },
+  usePlatformFetch
+);
 
 onLaunch(() => {
   console.log("App Launch");
@@ -19,7 +30,7 @@ onHide(() => {
   console.log("App Hide");
 });
 
-function initTfjs() {
+async function initTfjs() {
   plugin.configPlugin(
     {
       // polyfill fetch function
@@ -35,6 +46,11 @@ function initTfjs() {
     ENABLE_DEBUG
   );
 
+  await tf.setBackend("wasm");
+  console.log(tf.getBackend()); // wasm
+  tf.tensor([1, 2, 3, 4]).print(); // Tensor [1, 2, 3, 4]
+
+  await tf.setBackend("wechat-webgl");
   console.log(tf.getBackend()); // wechat-webgl
   tf.tensor([1, 2, 3, 4]).print(); // Tensor [1, 2, 3, 4]
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,40 @@ import uni from "@dcloudio/vite-plugin-uni";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  // plugins: [uni()],
+  plugins: [uni(), codeTransform()],
 });
+
+function codeTransform() {
+  return {
+    name: "codeTransform",
+    transform(code, file) {
+      // 修复wasm
+      if (
+        file.endsWith("tfjs-backend-wasm-threaded-simd.worker.js") ||
+        file.endsWith("tfjs-backend-wasm-threaded-simd.js")
+      ) {
+        code = code.replace(`require("worker_threads")`, "null");
+        code = code.replace(`require("perf_hooks")`, "null");
+      }
+      if (file.endsWith("backend_wasm.js")) {
+        code = code.replace(`env().getAsync('WASM_HAS_SIMD_SUPPORT')`, "false");
+        code = code.replace(
+          `env().getAsync('WASM_HAS_MULTITHREAD_SUPPORT')`,
+          "false"
+        );
+        code = code.replace(
+          `return (imports, callback) => {`,
+          `return (imports, callback) => {
+            WebAssembly.instantiate(path, imports).then(output => {
+                callback(output.instance, output.module);
+            });
+            return {};`
+        );
+      }
+      code = code.replace(`WebAssembly.`, `WXWebAssembly.`);
+      code = code.replace(`typeof WebAssembly`, `typeof WXWebAssembly`);
+      return { code };
+    },
+  };
+}
